a55506f8c6992835ed30b561bb6d395510e6e72a
Manjeetmathur <manjeetkumar62054@gmail.com>
2024-07-08 00:53:43 +0530

    new commit

--- Node_Tutoril/routes/menuRotues.js
+++ Node_Tutoril/routes/menuRotues.js
@@ -1,51 +1,42 @@
-const express = require('express')
-const router = express.Router()
-const menuItems = require("../models/Menu")
-const MenuItem = require('../models/Menu')
+const express = require("express");
+const router = express.Router();
+const menuItems = require("../models/Menu");
+const MenuItem = require("../models/Menu");
 
+router.post("/", async (req, res) => {
+  try {
+    const newMenu = new MenuItem(req.body);
 
-router.post('/', async(req, res) => {
-  try{
-       const newMenu = new Menu(req.body)
-    
     const response = await newMenu.save();
     //res.send(response)
-    console.log('data saved');
-     res.status(200).json(response);
-  }
-  catch(err){
+    console.log("data saved");
+    res.status(200).json(response);
+  } catch (err) {
     console.log(err);
-    res.status(500).json({error : 'Internal  server error'});
+    res.status(500).json({ error: "Internal  server error" });
   }
-  
-})
-router.get('/', async(req, res) => {
-       try{
-         const data = await MenuItem.find();
-         res.status(200).json(data);
-       }
-       catch(err){
-         console.log(err);
-         res.status(500).json({error : 'Internal  server error'});
-       }
-       
-})
-router.get('/:testtype', async(req, res) => {
-       try{
-         const testtype = req.params.testtyp;
-         if(testtype == 'sweet' || testtype == 'sour' || testtype == 'spicy'){
-           const response = await MenuItem.find({taste:testtype});
-           res.status(200).json(response);
-         }
-         else{
-           res.status(400).json({error : "invalid"});
-         }
-         
-       }
-       catch(err){
-         console.log(err);
-         res.status(500).json({error : 'Internal  server error'});
-       }
-       
-})
-module.exports = router
+});
+router.get("/", async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal  server error" });
+  }
+});
+router.get("/:testtype", async (req, res) => {
+  try {
+    const testtype = req.params.testtyp;
+    if (testtype == "sweet" || testtype == "sour" || testtype == "spicy") {
+      const response = await MenuItem.find({ taste:testtype });
+      res.status(200).json(response);
+    } else {
+      res.status(400).json({ error: "invalid" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal  server error" });
+  }
+});
+module.exports = router;
